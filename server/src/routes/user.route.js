32aff98183061a71b8a25839d3ceedaadf7c1379
talha-anxari <talhaanxaritn@outlook.com>
2025-07-12 15:57:20 +0500

    create user routes

---
+++ server/src/routes/user.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { followUser, getCurrentUser, getUserProfile, syncUser, updateProfile } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+// public routes
+router.get("/profile/:username", getUserProfile);
+
+// protected routes
+router.post("/sync", protectRoute, syncUser);
+router.post("/me", protectRoute, getCurrentUser);
+router.put("/profile", protectRoute, updateProfile);
+router.post("/follow/:targetUserId", protectRoute, followUser);
+export default router;
