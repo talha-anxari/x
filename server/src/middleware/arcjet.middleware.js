e43643c7da1dca7e016756bd7d263e9698bed783
talha-anxari <talhaanxaritn@outlook.com>
2025-07-13 20:25:54 +0500

    clear extra code

--- server/src/middleware/arcjet.middleware.js
+++ server/src/middleware/arcjet.middleware.js
@@ -1,34 +1,4 @@
 import { aj } from "../config/arcjet.js";
-// Arcjet middleware for rate limiting, bot protection, and security
-// export const arcjetMiddleware = async(req, res, next) => {
-//     try {
-//         const decision = await aj.protect(req, {
-//             requested: 1, // each request counts as 1 unit
-//         });
-//         if(decision.isDenied()){
-//             if(decision.reason.isRateLimit()){
-//                 return res.status(429).json({
-//                     error: "Bot access denied",
-//                     message: "Rate limit exceeded. Please try again later.",
-//                 })
-//             }else if(decision.reason.isBot()){
-//                 return res.status(403).json({
-//                     error: "Bot access denied",
-//                     message: "Access denied for bots.",
-//                 });
-//             }else{
-//                 return res.status(403).json({
-//                     error: "Access denied",
-//                     message: decision.reason.message,
-//                 });
-//             }
-//         }
-        
-//     } catch (error) {
-//         console.error("Arcjet middleware error:", error);
-//         res.status(500).send("Internal Server Error");
-//     }
-// };
 
 export const arcjetMiddleware = async (req, res, next) => {
   try {
