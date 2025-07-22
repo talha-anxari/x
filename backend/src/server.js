03c51a657e7b58cf56472945e1c58fcca31dfb2e
talha-anxari <talhaanxaritn@outlook.com>
2025-07-23 03:26:16 +0500

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,17 @@
-
-// const startServer = async () => {
-//   try {
-//     await connectDB();
-
-//     // listen for local development
-//     if (ENV.NODE_ENV !== "production") {
-//       app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
-//     }
-//   } catch (error) {
-//     console.error("Failed to start server:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
-// // export for vercel
-// export default app;
-
-
 import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import userRoutes from "./routes/user.route.js";
+
 import { ENV } from "./config/env.js"
 import { connectDB } from "./config/db.js"
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello from server 😎"))
+app.use(clerkMiddleware());
+app.get("/", (req, res) => res.send("Hello from server 😎"));
+app.use("/api/users", userRoutes)
 
 const startServer = async () => {
   try {
