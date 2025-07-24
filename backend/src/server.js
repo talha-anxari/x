9fcf8923dac94ef0b13dd570c5030ed25f3767c0
talha-anxari <talhaanxaritn@outlook.com>
2025-07-25 01:51:55 +0500

    notification and arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,27 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
-import { ENV } from "./config/env.js"
-import { connectDB } from "./config/db.js"
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
+
 app.get("/", (req, res) => res.send("Hello from server 😎"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
