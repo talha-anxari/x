b261f6c7db03e770998b5ad8120756bd489530da
talha-anxari <talhaanxaritn@outlook.com>
2025-07-25 01:31:16 +0500

    comment routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 import { ENV } from "./config/env.js"
 import { connectDB } from "./config/db.js"
@@ -15,6 +16,7 @@ app.get("/", (req, res) => res.send("Hello from server 😎"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
